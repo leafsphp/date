c46e583eeb209bb4c572e8147422a4cde532935e
mychidarko <mickdd22@gmail.com>
2023-03-31 17:24:33 +0000

    feat: add startOf method

--- src/Date.php
+++ src/Date.php
@@ -17,75 +17,100 @@ use Leaf\Date\Utils;
  */
 class Date
 {
-	/**Date entered by a user */
-	protected string $userDate;
+    /**Date entered by a user */
+    protected string $userDate;
 
-	/**Parsed user date */
-	protected string $parsedDate;
+    /**Parsed user date */
+    protected string $parsedDate;
 
-	/**PHP datetime instance */
-	protected DateTime $date;
+    /**PHP datetime instance */
+    protected DateTime $date;
 
-	/**Date format entered by a user */
-	protected string $userFormat;
+    /**Date format entered by a user */
+    protected string $userFormat;
 
-	public function __construct()
-	{
-		$this->tick();
-		$this->format();
-	}
+    public function __construct()
+    {
+        $this->tick();
+        $this->format();
+    }
 
-	/**
-	 *
-	 */
-	public function tick(string $userDate = 'now', string $userTimeZone = null): Date
-	{
-		$this->userDate = $userDate;
-		$this->date = new DateTime(str_replace('/', '-', $userDate));
+    /**
+     *
+     */
+    public function tick(string $userDate = 'now', string $userTimeZone = null): Date
+    {
+        $this->userDate = $userDate;
+        $this->date = new DateTime(str_replace('/', '-', $userDate));
 
-		if ($userTimeZone) {
-			$this->setTimezone($userTimeZone);
-		}
+        if ($userTimeZone) {
+            $this->setTimezone($userTimeZone);
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Set default date timezone
-	 */
-	public function setTimezone(String $timezone = "Africa/Accra")
-	{
-		$this->date->setTimezone(new \DateTimeZone($timezone));
+    /**
+     * Set default date timezone
+     */
+    public function setTimezone(String $timezone = "Africa/Accra")
+    {
+        $this->date->setTimezone(new \DateTimeZone($timezone));
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Add a duration to the current date
-	 */
-	public function add($duration, string $interval = null): Date
-	{
-		$this->date->modify($interval ? "$duration $interval" : $duration);
+    /**
+     * Add a duration to the current date
+     */
+    public function add($duration, string $interval = null): Date
+    {
+        $this->date->modify($interval ? "$duration $interval" : $duration);
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Subtract a duration to the current date
-	 */
-	public function subtract($duration, string $interval = null): Date
-	{
-		return $this->add($interval ? "-$duration $interval" : '-' . $duration);
-	}
+    /**
+     * Subtract a duration to the current date
+     */
+    public function subtract($duration, string $interval = null): Date
+    {
+        return $this->add($interval ? "-$duration $interval" : '-' . $duration);
+    }
 
-	/**
-	 * Get the formatted date according to the string of tokens passed in.
-	 */
-	public function format(string $format = 'c'): string
-	{
-		$this->userFormat = Utils::formatToPHP($format);
-		$this->parsedDate = $this->date->format($this->userFormat);
+    /**
+     * Get the start of a time unit
+     */
+    public function startOf(string $unit): Date
+    {
+        $units = [
+            'year' => 'Y-01-01 00:00:00',
+            'month' => 'Y-m-01 00:00:00',
+            'week' => 'Y-m-d 00:00:00',
+            'day' => 'Y-m-d 00:00:00',
+            'hour' => 'Y-m-d H:00:00',
+            'minute' => 'Y-m-d H:i:00',
+            'second' => 'Y-m-d H:i:s',
+        ];
 
-		return $this->parsedDate;
-	}
+        $this->date->modify(date(
+            $units[$unit],
+            $unit === 'week' ?
+                strtotime("this week", $this->date->getTimestamp()) :
+                $this->date->getTimestamp()
+        ));
+
+        return $this;
+    }
+
+    /**
+     * Get the formatted date according to the string of tokens passed in.
+     */
+    public function format(string $format = 'c'): string
+    {
+        $this->userFormat = Utils::formatToPHP($format);
+        $this->parsedDate = $this->date->format($this->userFormat);
+
+        return $this->parsedDate;
+    }
 }
