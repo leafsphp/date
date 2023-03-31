99f6e4150343fb6846a27c1c3c3b2c5f5366c409
mychidarko <mickdd22@gmail.com>
2023-03-31 19:59:10 +0000

    chore: update code setup

--- src/Date.php
+++ src/Date.php
@@ -133,7 +133,44 @@ class Date
      */
     public function format(string $format = 'c'): string
     {
-        $this->userFormat = Utils::formatToPHP($format);
+		$matches = [
+			'YY' => 'y',
+			'YYYY' => 'Y',
+			'M' => 'n',
+			'MM' => 'm',
+			'MMM' => 'M',
+			'MMMM' => 'F',
+			'D' => 'j',
+			'DD' => 'd',
+			'd' => 'w',
+			'dd' => 'D',
+			'ddd' => 'D',
+			'dddd' => 'l',
+			'H' => 'G',
+			'HH' => 'H',
+			'h' => 'g',
+			'hh' => 'h',
+			'a' => 'a',
+			'A' => 'A',
+			'm' => 'i',
+			'mm' => 'i',
+			's' => 's',
+			'ss' => 's',
+			'SSS' => 'u',
+			'Z' => 'Z',
+			'T' => '\T',
+		];
+
+        $this->userFormat = preg_replace_callback('/\[([^\]]+)]|Y{1,4}|T|M{1,4}|D{1,2}|d{1,4}|H{1,2}|h{1,2}|a|A|m{1,2}|s{1,2}|Z{1,2}|SSS/', function ($match) use ($matches) {
+			if (strpos($match[0], '[') === 0) {
+				return preg_replace_callback('/\[(.*?)\]/', function ($matched) {
+					return preg_replace("/(.)/", "\\\\$1", $matched[1]);
+				}, $match[0]);
+			}
+
+			return $matches[$match[0]] ?? $match[0];
+		}, $format);
+
         $this->parsedDate = $this->date->format($this->userFormat);
 
         return $this->parsedDate;
